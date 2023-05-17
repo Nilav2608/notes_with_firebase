35d782b2d0743db625889e0fbcdbc2ff37b99774
Nilavarasu2608 <knlsu444@gmail.com>
2023-05-18 00:25:05 +0530

    home app bar search and info icons

--- lib/home.dart
+++ lib/home.dart
@@ -8,23 +8,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Color bgColor = const Color(0x252525) ;
+  Color bgColor = const Color(0x252525);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgColor,
       appBar: AppBar(
         backgroundColor: bgColor,
+        elevation: 0,
         title: const Padding(
           padding: EdgeInsets.all(8.0),
           child: Text(
             "Notes",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 43
-              ),
-            ),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+          ),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 59, 59, 59),
+                    borderRadius: BorderRadius.circular(15)),
+                child: const Icon(
+                  Icons.search_rounded,
+                  size: 24,
+                )),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 59, 59, 59),
+                    borderRadius: BorderRadius.circular(15)),
+                child: const Icon(
+                  Icons.info_outline,
+                  size: 24,
+                )),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+        ],
       ),
     );
   }
