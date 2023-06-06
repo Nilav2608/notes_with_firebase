05e5715c58dcc3ce6381aa1f6c95186410d81fe8
Nilavarasu2608 <knlsu444@gmail.com>
2023-06-06 21:02:37 +0530

    firebase initialized and added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_with_firebase/firebase_options.dart';
 import 'package:notes_with_firebase/view/pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options:DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
@@ -10,23 +16,21 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   TextTheme _buildTextTheme(TextTheme base) {
-  return GoogleFonts.nunitoTextTheme(base).copyWith(
-    // Customize other TextTheme properties as needed
-  );
-}
-
+    return GoogleFonts.nunitoTextTheme(base).copyWith(
+        // Customize other TextTheme properties as needed
+        );
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        textTheme: _buildTextTheme(ThemeData.light().textTheme),
-      ),
-      home: const HomePage()
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: _buildTextTheme(ThemeData.light().textTheme),
+        ),
+        home: const HomePage());
   }
 }
