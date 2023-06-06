bd01aac2e763dc8a181d77fb573c75be4226081e
Nilavarasu2608 <knlsu444@gmail.com>
2023-06-06 23:59:35 +0530

    intro page

---
+++ lib/view/pages/intro_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0x00252525),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                "Welcome To Notes",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                "Please login to your account or create new account to continue",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Image.asset("assets/introNote.png", height: 200),
+              const SizedBox(
+                height: 100,
+              ),
+              SizedBox(
+                height: 48,
+                width: 347,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: const ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll(Color(0xFF8875FF))),
+                    child: const Text("LOGIN")),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                height: 48,
+                width: 347,
+                child: OutlinedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      side: MaterialStateProperty.all<BorderSide>(
+                        const BorderSide(
+                          color: Color(
+                              0xFF8E7CFF), // Set the desired outline color
+                          width: 2.0, // Set the desired outline width
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "CREATE ACCOUNT",
+                      style: TextStyle(color: Colors.white),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
