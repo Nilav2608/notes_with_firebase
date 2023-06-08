2a89c003345da2a5c10235df6a4458cf9b8a68cf
Nilavarasu2608 <knlsu444@gmail.com>
2023-06-08 14:55:09 +0530

    textfields

---
+++ lib/view/utils/text_fields.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class TextFields extends StatelessWidget {
+  const TextFields({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
